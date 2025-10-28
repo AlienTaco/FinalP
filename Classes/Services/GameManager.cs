1627090be4e87637ddb12d4ac6c80b4a27335ce0
אייל זהר <eyalandamir@gmail.com>
2025-10-28 12:48:35 +0200

    ...

---
+++ Classes/Services/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using FinalP.Classes.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace FinalP.Classes.Services
+{
+    public class GameManager
+    {
+        private readonly Canvas gameCanvas;
+        private readonly List<GameObject> objects = new();
+
+        public int Rows { get; } = 5;
+        public int Columns { get; } = 7;
+        public double CellSize { get; } = 50;
+
+        public GameManager(Canvas canvas)
+        {
+            gameCanvas = canvas;
+        }
+
+        public void AddObject(GameObject obj)
+        {
+            objects.Add(obj);
+            obj.CreateVisual(gameCanvas, Colors.Gray);
+        }
+
+        public void RemoveObject(GameObject obj)
+        {
+            obj.Destroy(gameCanvas);
+            objects.Remove(obj);
+        }
+
+        public void ClearAll()
+        {
+            foreach (var obj in objects)
+                obj.Destroy(gameCanvas);
+            objects.Clear();
+        }
+
+        public (double x, double y) GetCellPosition(int row, int col)
+        {
+            return (col * CellSize, row * CellSize);
+        }
+    }
+}
+     
---
+++ Classes/Templates/GameObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace FinalP.Classes.Templates
+{
+    public abstract class GameObject
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; } = 50;
+        public double Height { get; set; } = 50;
+        public bool IsActive { get; set; } = true;
+
+        protected Rectangle Visual;
+
+        public virtual void CreateVisual(Canvas canvas, Color color)
+        {
+            Visual = new Rectangle
+            {
+                Width = Width,
+                Height = Height,
+                Fill = new SolidColorBrush(color)
+            };
+
+            Canvas.SetLeft(Visual, X);
+            Canvas.SetTop(Visual, Y);
+            canvas.Children.Add(Visual);
+        }
+
+        public virtual void UpdatePosition()
+        {
+            if (Visual == null) return;
+            Canvas.SetLeft(Visual, X);
+            Canvas.SetTop(Visual, Y);
+        }
+
+        public virtual void Destroy(Canvas canvas)
+        {
+            if (Visual != null)
+            {
+                canvas.Children.Remove(Visual);
+                Visual = null;
+            }
+        }
+    }
+}
